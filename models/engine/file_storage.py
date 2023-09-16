caa5e0b22e093fb3c0384e8c0f473e27fe39f494
Jess <jessgatura@gmail.com>
2023-09-17 01:56:09 +0300

    fixed pycodestyle error

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -11,6 +11,7 @@ from models.review import Review
 from models.state import State
 from models.user import User
 
+
 class FileStorage:
     """filestorage class definition"""
     __file_path = "file.json"
